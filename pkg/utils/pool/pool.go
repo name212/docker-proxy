0177fbbbcd2ad2cc0deaf078a4d6a5d3a9230f1a
Nikolay Mitrofanov <n.mitrofanow@gmail.com>
2026-08-12 23:12:15 +0300

    ++

--- pkg/utils/pool/pool.go
+++ pkg/utils/pool/pool.go
@@ -1,3 +1,6 @@
+// Copyright 2026
+// license that can be found in the LICENSE file.
+
 package pool
 
 import "sync"
--- pkg/utils/strings/sort.go
+++ pkg/utils/strings/sort.go
@@ -1,3 +1,6 @@
+// Copyright 2026
+// license that can be found in the LICENSE file.
+
 package strings
 
 func SortFuncAsc(i, j string) int {
